1e33c99b45db2c8826383870066662d962e76faa
harshpratap092507 <78509544+harshpratap092507@users.noreply.github.com>
2023-09-22 16:39:53 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # QKart
+# QKart
---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Avatar, Button, Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import React from "react";
+import "./Header.css";
+import { useHistory } from "react-router-dom";
+
+const Header = ({ children, hasHiddenAuthButtons }) => {
+
+    const history = useHistory()
+    if (hasHiddenAuthButtons) {
+      return (
+        <Box className="header">
+          <Box className="header-title">
+              <img src="logo_light.svg" alt="QKart-icon"></img>
+          </Box>
+          {children}
+          <Button
+            className="explore-button"
+            startIcon={<ArrowBackIcon />}
+            variant="text"
+            onClick={() => {
+              history.push('/')
+            }}
+          >
+            Back to explore
+          </Button>
+        </Box>
+      );
+    }
+
+    return (
+      <Box className="header">
+        <Box className="header-title">
+            <img src="logo_light.svg" alt="QKart-icon"></img>
+        </Box>
+        {children}
+        <Stack direction="row" spacing={1} alignItems="center">
+        {localStorage.getItem('username') ? <>
+        <Avatar src="avatar.png" alt={localStorage.getItem('username')}/>
+        <p>{localStorage.getItem('username')}</p>
+        <Button onClick={() => {
+              localStorage.clear()
+              window.location.reload()
+            }}>Logout</Button>
+          </> 
+          : (<>
+          <Button onClick={() => {
+              history.push('/login')
+            }}>Login</Button>
+          <Button variant='contained' onClick={() => {
+              history.push('/register')
+            }}>Register</Button>
+            </>)}
+        </Stack>
+      </Box>
+    );
+    
+};
+
+export default Header;
